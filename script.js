807c3ea88a5b92618f1578ec13c5bfa5a9921208
kauephe <165967721+kauephe@users.noreply.github.com>
2024-05-29 23:46:49 +0000

    29/05/24

---
+++ script.js
@@ -0,0 +1,47 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+  {
+    Enuciado: "Pergunta 1",
+    alternativas: [
+      "Resposta 1",
+      "Resposta 2",
+    ]
+  },
+
+  {
+    Enuciado: "Pergunta 2",
+    alternativas: [
+      "Resposta 1",
+      "Resposta 2",
+    ]
+  },
+
+  {
+    Enuciado: "Pergunta 3",
+    alternativas: [
+      "Resposta 1",
+      "Resposta 2",
+    ]
+  },
+
+  {
+    Enuciado: "Pergunta 4",
+    alternativas: [
+      "Resposta 1",
+      "Resposta 2",
+    ]
+  },
+
+  {
+    Enuciado: "Pergunta 5",
+    alternativas: [
+      "Resposta 1",
+      "Resposta 2",
+    ]
+  }
+];
